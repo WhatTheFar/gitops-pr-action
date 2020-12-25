692091fe79c94a6b2ba25dd00c3790f19fe0548e
WhatTheFar <jakpat.m@gmail.com>
2020-12-26 05:27:21 +0700

    feat(tools): kustomize & gomplate installation

---
+++ src/tools.ts
@@ -0,0 +1,39 @@
+import * as tc from '@actions/tool-cache';
+import * as core from '@actions/core';
+
+const KUSTOMIZE_VERSION = 'v3.8.8';
+const GOMPLATE_VERSION = 'v3.8.0';
+
+export async function installKustomize(): Promise<void> {
+  const platform = process.platform;
+  const arch = 'amd64';
+
+  const path = await tc.downloadTool(
+    `https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2F${KUSTOMIZE_VERSION}/kustomize_${KUSTOMIZE_VERSION}_${platform}_${arch}.tar.gz`,
+  );
+  const extractedDir = await tc.extractTar(path);
+  const cachedPath = await tc.cacheDir(
+    extractedDir,
+    'kustomize',
+    KUSTOMIZE_VERSION,
+  );
+
+  core.addPath(cachedPath);
+}
+
+export async function installGomplate(): Promise<void> {
+  const platform = process.platform;
+  const arch = 'amd64';
+
+  const path = await tc.downloadTool(
+    `https://github.com/hairyhenderson/gomplate/releases/download/${GOMPLATE_VERSION}/gomplate_${platform}-${arch}-slim`,
+  );
+  const cachedFile = await tc.cacheFile(
+    path,
+    'gomplate',
+    'gomplate',
+    KUSTOMIZE_VERSION,
+  );
+
+  core.addPath(cachedFile);
+}
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,43 @@
+import * as exec from '@actions/exec';
+
 export const isString = (a: unknown): a is string => typeof a === 'string';
 
 export const isObject = (
   a: unknown,
 ): a is Record<string | number | symbol, unknown> => a instanceof Object;
+
+export type ExecOptions = Omit<exec.ExecOptions, 'listeners'>;
+
+export interface ExecResult {
+  stdout: string;
+  stderr: string;
+  exitCode: number;
+}
+
+export async function execCmd(
+  commandLine: string,
+  args?: string[],
+  options?: ExecOptions,
+): Promise<ExecResult> {
+  const stdout: string[] = [];
+  const stderr: string[] = [];
+  const newOptions: exec.ExecOptions = {
+    ...options,
+    listeners: {
+      stdout: (data: Buffer) => {
+        stdout.push(data.toString());
+      },
+      stderr: (data: Buffer) => {
+        stderr.push(data.toString());
+      },
+    },
+  };
+
+  const exitCode = await exec.exec(commandLine, args, newOptions);
+
+  return {
+    exitCode,
+    stdout: stdout.join(''),
+    stderr: stderr.join(''),
+  };
+}
