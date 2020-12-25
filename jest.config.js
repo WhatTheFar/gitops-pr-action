ce79e6c89da67bab0208f0209a9b52a0f9743969
WhatTheFar <jakpat.m@gmail.com>
2020-12-25 17:37:24 +0700

    chore: init jest config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+// jest.config.js
+
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.spec.ts$',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+};
