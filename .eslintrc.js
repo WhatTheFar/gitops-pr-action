650608e921910e4f43c0656f0f3e9dc55fb65bf8
WhatTheFar <jakpat.m@gmail.com>
2021-01-06 17:41:34 +0700

    chore(eslint): enable eslint-plugin-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier',
+    'plugin:prettier/recommended',
     'prettier/@typescript-eslint',
     'plugin:import/errors',
     'plugin:import/warnings',
