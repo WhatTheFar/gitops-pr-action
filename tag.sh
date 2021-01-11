16ba2c4180f2d7d5226e25f07fd920bbb2dc5826
WhatTheFar <jakpat.m@gmail.com>
2021-01-11 15:06:29 +0700

    script: move tags

--- tag.sh
+++ tag.sh
@@ -4,17 +4,21 @@ RESET=`tput sgr0`
 GREEN=`tput setaf 34`
 BLUE=`tput setaf 32`
 
-tag=$1
+for tag in "$@"; do
 
-echo -e "$BLUE--- Delete a local Git tag $file ---$RESET"
-git tag -d $tag
-echo
-echo -e "$BLUE--- Delete a remote Git tag $file ---$RESET"
-git push --delete origin $tag
+    echo -e "$GREEN== TAG: $tag $RESET"
 
-echo
-echo -e "$BLUE--- Create a new local tag ---$RESET"
-git tag $tag
-echo
-echo -e "$BLUE--- Push the tag to remote ---$RESET"
-git push origin $tag
+    echo -e "$BLUE--- Delete a local Git tag $tag ---$RESET"
+    git tag -d $tag
+    echo
+    echo -e "$BLUE--- Delete a remote Git tag $tag ---$RESET"
+    git push --delete origin $tag
+
+    echo
+    echo -e "$BLUE--- Create a new local tag $tag ---$RESET"
+    git tag $tag
+    echo
+    echo -e "$BLUE--- Push the tag to remote $tag ---$RESET"
+    git push origin $tag
+
+done
